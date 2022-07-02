86b7041e5fb4cca92179e7870736d4628b61f02e
suman <ronaldosuman97@gmail.com>
2022-07-02 20:13:52 +0530

    joins performed

--- dist/routes/post.js
+++ dist/routes/post.js
@@ -2,8 +2,35 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const client_1 = require("@prisma/client");
-const { user, post } = new client_1.PrismaClient();
+const { user, post, like } = new client_1.PrismaClient();
 const router = (0, express_1.Router)();
+router.get("/", async (req, res) => {
+    const posts = await post.findMany({
+        select: {
+            id: true,
+            author: true,
+            caption: true,
+            createdAt: true,
+            image: true,
+            Like: {
+                select: {
+                    userId: true
+                }
+            }
+        }
+    });
+    res.status(200).send(posts);
+});
+router.post("/like", async (req, res) => {
+    const { postId, userId } = req.body;
+    const newLike = await like.create({
+        data: {
+            profile: userId,
+            post: postId,
+        }
+    });
+    res.status(200).send(newLike);
+});
 router.post("/", async (req, res) => {
     const { userId, username, caption, image } = req.body;
     const userExists = await user.findUnique({
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,7 +1,40 @@
 import { Router, Response, Request } from "express";
 import { PrismaClient } from "@prisma/client"
-const { user, post } = new PrismaClient();
+const { user, post, like } = new PrismaClient();
 const router = Router();
+
+
+
+router.get("/", async (req: Request, res: Response) => {
+    const posts = await post.findMany({
+        select: {
+            id: true,
+            author: true,
+            caption: true,
+            createdAt: true,
+            image: true,
+            Like: {
+                select: {
+                    userId: true
+                }
+            }
+        }
+
+    })
+    res.status(200).send(posts)
+
+})
+router.post("/like", async (req: Request, res: Response) => {
+    const { postId, userId } = req.body;
+    const newLike = await like.create({
+        data: {
+            profile: userId,
+            post: postId,
+        }
+    })
+
+    res.status(200).send(newLike)
+})
 
 router.post("/", async (req: Request, res: Response) => {
     const { userId, username, caption, image } = req.body;
